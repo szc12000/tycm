112fc616ea87b4efce11ed788495b599df452f28
szc12000 <szc12000@163.com>
2016-12-24 15:06:56 +0800

    second commit

---
+++ Ceshi.Class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @Author: Marte
+ * @Date:   2016-12-24 14:59:20
+ * @Last Modified by:   Marte
+ * @Last Modified time: 2016-12-24 15:03:55
+ */
+class Ceshi {
+    private $name = 'ceshi';
+    public function __construct($name) {
+        if (empty($name)) {
+            echo '不能用空值初始化ceshi类';
+            exit;
+        }
+        $this->name = $name;
+    }
+
+    public function say() {
+        echo 'this name is ',$this->name;
+    }
+}
--- index.php
+++ index.php
@@ -3,7 +3,8 @@
  * @Author: szc
  * @Date:   2016-12-24 13:44:04
  * @Last Modified by:   Marte
- * @Last Modified time: 2016-12-24 13:45:14
+ * @Last Modified time: 2016-12-24 15:04:56
  */
 
-phpinfo();
+$nameObj = new Ceshi('linken');
+$nameObj->say();
